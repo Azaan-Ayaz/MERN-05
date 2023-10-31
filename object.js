68526fc6f7feaa20de531044edef1e9f1e68540f
Abdul Ghani <148964975+Azaan-Ayaz@users.noreply.github.com>
2023-10-31 18:59:50 +0500

    new file

---
+++ object.js
@@ -0,0 +1,46 @@
+/*const personName = "Abdullah";
+const personGender = "Male";
+const personCity = "Sukkur";
+
+const person_2Name = "Abdullah";
+const person_2Gender = "Male";
+const person_2City = "Sukkur";
+
+const person_3Name = "xyz";
+const person_3Gender = "Female";
+const person_3City = "Sukkur";
+
+
+const personsName = [personName,person_2Name,person_3Name];
+const personsGender = [personGender,person_2Gender,person_3Gender];
+const personsCity = [personCity,person_2City,person_3City];*/
+
+const person_1 = {
+    name:"M.Umar",
+    city: "Shikarpur",
+}
+
+const person_2 = {
+    name:"Abdulah",
+    address: "ku2",
+    city: "Sukkur",
+}
+
+const person_3 = {
+    name:"Abu bakar",
+    city: "Sukkur",
+}
+
+console.log(person_2.address ?. includes("2"));
+
+/*const persons = [person_1,person_2,person_3];
+console.log(persons[0]);*/
+
+/*const myName = "   Dot  Developer    ";
+console.log(myName.trim().length);
+
+const dev = "(Entwikler)";
+const myName = "DotDeveloper".contact(dev);
+
+console.log(myName .includes("i"));*/
+
