57b2389b366380efbe637fdc96e2bc8155273900
Abdul Ghani <148964975+Azaan-Ayaz@users.noreply.github.com>
2023-10-31 17:24:33 +0500

    Add files via upload

---
+++ W2D2/app.js
@@ -0,0 +1,80 @@
+function xyz(){
+console.log("=================")
+console.log("==============")
+console.log("===============")
+
+}
+
+//-----
+xyz();
+xyz();
+xyz();
+
+//------
+
+for(let i=0; i<=10;i++){
+    console.log(i);
+}
+
+let a = "string";
+let arr = Array .from(a)
+console.log(arr);
+
+let num =[22,32,25,40]
+for (let i of num){
+console.log(i);};
+
+let d=[5,8,6,9]
+for( let i of d){
+    console.log(i)
+}
+
+let j = 0;
+if(j = 0){
+    console.log("j is zero");
+}
+else console.log("j is not zero");
+
+let x = 2;
+    b = 3;
+    c = (x + b);
+    console.log (c);
+if (c == 5){
+console.log("sum is correct");
+}
+else if (c == 5) {
+    console.log("matched");
+}
+ else {console.log("answer is incorrect");}
+
+
+// switch condition
+
+
+let day = 1;
+switch (day) {
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case  6:
+    day = "Saturday";
+    case 7:
+    day = "Sunday";
+    break;
+}
+console.log(day)
+
+
+
